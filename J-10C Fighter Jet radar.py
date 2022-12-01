46a007917d63ea0af1b92259215d9ead5a602880
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 10:22:42 +0500

    Create J-10C Fighter Jet radar.py

---
+++ J-10C Fighter Jet radar.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+format(1473, '#b'), format(1437, 'b')
+('0b10111000001', '10110011101')
+f'{1473:#b}', f'{1473:b}'
+('0b10111000001', '10111000001')
